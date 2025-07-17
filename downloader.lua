d94a5d6c33530171efbdf5e9929b41b0b682225e
Justin Stockton <justinstockton67@gmail.com>
2025-07-17 09:25:14 -0400

    downloader

---
+++ downloader.lua
@@ -0,0 +1,28 @@
+local user = "Justin-Stockton"
+local repo = "minecraft_turtle"
+local branch = "main"
+
+local args = { "downloader" }
+if #args < 1 then
+	print("Usage: downloader <program_name>")
+	return
+end
+
+local programName = args[1]
+local url = string.format("https://raw.githubusercontent.com/%s/%s/%s/%s.lua", user, repo, branch, programName)
+
+print("Downloading '" .. programName .. "' from GitHub...")
+local response = http.get(url)
+
+if response and response.getResponseCode() == 200 then
+	local content = response.readAll()
+	local file = fs.open(programName, "w")
+	file.write(content)
+	file.close()
+	print("Successfully downloaded and saved as '" .. programName .. "'")
+else
+	print("Error: Could not download file.")
+	if response then
+		print("HTTP Status: " .. response.getResponseCode())
+	end
+end
